97b03c2b6d9ab4672d015332d70f580723e716aa
Maxime Audinot <35561814+maudinot@users.noreply.github.com>
2020-12-28 19:16:07 +0100

    Ajoutez des fichiers projet.

---
+++ ImpoLobbyCommands/GameEventListener.cs
@@ -0,0 +1,122 @@
+﻿using Impostor.Api.Events;
+using Impostor.Api.Events.Player;
+using Impostor.Api.Innersloth;
+using Impostor.Api.Net.Inner.Objects;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Impostor.Plugins.LobbyCommands.Handlers
+{
+    /// <summary>
+    ///     A class that listens for two events.
+    /// </summary>
+    class GameEventListener : IEventListener
+    {
+        private readonly ILogger<LobbyCommandsPlugin> _logger;
+
+        public GameEventListener(ILogger<LobbyCommandsPlugin> logger)
+        {
+            _logger = logger;
+        }
+
+        [EventListener]
+        public void OnGameStarted(IGameStartedEvent e)
+        {
+            _logger.LogInformation($"Game is starting.");
+        }
+
+        [EventListener]
+        public void OnGameEnded(IGameEndedEvent e)
+        {
+            _logger.LogInformation($"Game has ended.");
+        }
+
+        [EventListener]
+        public async ValueTask OnPlayerChat(IPlayerChatEvent e)
+        {
+            if (e.Game.GameState == GameStates.NotStarted && e.ClientPlayer.IsHost)
+            {
+                if (e.Message.StartsWith("/help"))
+                {
+                    await ServerSendChatAsync("Commands list: /map, /impostors", e.ClientPlayer.Character);
+
+                }
+
+                if (e.Message.StartsWith("/map "))
+                {
+                    switch (e.Message.ToLowerInvariant()[5..])
+                    {
+                        case "skeld":
+                            if(e.Game.Options.Map == MapTypes.Skeld)
+                            {
+                                await ServerSendChatAsync("Map is already Skeld!", e.ClientPlayer.Character, true);
+                            }
+                            else
+                            {
+                                e.Game.Options.Map = MapTypes.Skeld;
+                                await e.Game.SyncSettingsAsync();
+                                await ServerSendChatAsync("Map changed to Skeld", e.ClientPlayer.Character, true);
+                            }
+                            break;
+                        case "mira":
+                        case "mirahq":
+                        case "mira hq":
+                            if (e.Game.Options.Map == MapTypes.MiraHQ)
+                            {
+                                await ServerSendChatAsync("Map is already MiraHQ!", e.ClientPlayer.Character, true);
+                            }
+                            else
+                            {
+                                e.Game.Options.Map = MapTypes.MiraHQ;
+                                await e.Game.SyncSettingsAsync();
+                                await ServerSendChatAsync("Map changed to MiraHQ", e.ClientPlayer.Character, true);
+                            }
+                            break;
+                        case "polus":
+                            if (e.Game.Options.Map == MapTypes.Polus)
+                            {
+                                await ServerSendChatAsync("Map is already Polus!", e.ClientPlayer.Character, true);
+                            }
+                            else
+                            {
+                                e.Game.Options.Map = MapTypes.Polus;
+                                await e.Game.SyncSettingsAsync();
+                                await ServerSendChatAsync("Map changed to Polus", e.ClientPlayer.Character, true);
+                            }
+                            break;
+                    }
+                    
+                }
+                if (e.Message.StartsWith("/impostors "))
+                {
+                    int num;
+                    if (!int.TryParse(e.Message[11].ToString(), out num))
+                    {
+                        await ServerSendChatAsync("Invalid input: expected a number", e.ClientPlayer.Character);
+                    } else
+                    {
+                        e.Game.Options.NumImpostors = num;
+                        await e.Game.SyncSettingsAsync();
+                        await ServerSendChatAsync("Impostor count set to " + e.Message[11], e.ClientPlayer.Character, true);
+                    }
+                }
+            }
+            //_logger.LogInformation($"{e.PlayerControl.PlayerInfo.PlayerName} said {e.Message}");
+        }
+
+        private async ValueTask ServerSendChatAsync(string text, IInnerPlayerControl player, bool toPlayer = false)
+        {
+            string playername = player.PlayerInfo.PlayerName;
+            await player.SetNameAsync($"Server");
+            if (toPlayer)
+            {
+                await player.SendChatToPlayerAsync($"{text}");
+            }
+            else
+            {
+                await player.SendChatAsync($"{text}");
+            }
+            await player.SetNameAsync(playername);
+        }
+    }
+}
---
+++ ImpoLobbyCommands/LobbyCommandsPlugin.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using Impostor.Api.Events.Managers;
+using Impostor.Api.Plugins;
+using Impostor.Plugins.LobbyCommands.Handlers;
+using Microsoft.Extensions.Logging;
+
+namespace Impostor.Plugins.LobbyCommands
+{
+    [ImpostorPlugin(
+        package: "gg.impostor.lobbycommands",
+        name: "LobbyCommands",
+        author: "Cybours",
+        version: "1.0.0")]
+    public class LobbyCommandsPlugin : PluginBase
+    {
+        /// <summary>
+        ///     A logger that works seamlessly with the server.
+        /// </summary>
+        private readonly ILogger<LobbyCommandsPlugin> _logger;
+
+        private readonly IEventManager _eventManager;
+        private IDisposable _unregister;
+
+        public LobbyCommandsPlugin(ILogger<LobbyCommandsPlugin> logger, IEventManager eventManager)
+        {
+            _logger = logger;
+            _eventManager = eventManager;
+        }
+
+        /// <summary>
+        ///     This is called when your plugin is enabled by the server.
+        /// </summary>
+        /// <returns></returns>
+        public override ValueTask EnableAsync()
+        {
+            _logger.LogInformation("LobbyCommands is being enabled.");
+            _unregister = _eventManager.RegisterListener(new GameEventListener(_logger));
+            return default;
+        }
+
+        /// <summary>
+        ///     This is called when your plugin is disabled by the server.
+        ///     Most likely because it is shutting down.
+        /// </summary>
+        /// <returns></returns>
+        public override ValueTask DisableAsync()
+        {
+            _logger.LogInformation("LobbyCommands is being disabled.");
+            _unregister.Dispose();
+            return default;
+        }
+    }
+}
